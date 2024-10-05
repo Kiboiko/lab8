008d6e7f8ce39aa9d7024cbc21cdf8b70fa9855c
kiboiko <kirboy2013@gmail.com>
2024-10-05 09:01:52 +0300

    Количество дней

--- time.pas
+++ time.pas
@@ -1,10 +1,15 @@
 ﻿begin
   var x := ReadInteger('Введите год:');
+  var t := false;
   if (x mod 4 = 0) then
   begin
     if ((x mod 100 = 0) and (x mod 400 <> 0)) then
-      print('Не високосный')
+      t := false
     else
-      print('Високосный');
+      t := true;
   end;
+  if t then
+    println(366)
+  else
+    println(365);
 end.
