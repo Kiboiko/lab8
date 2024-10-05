de2c75b2633c23c2e751248663057c2c3bd00a2b
kiboiko <kirboy2013@gmail.com>
2024-10-05 08:57:15 +0300

    visokosnyi

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var x := ReadInteger('Введите год:');
+  if (x mod 4 = 0) then
+  begin
+    if ((x mod 100 = 0) and (x mod 400 <> 0)) then
+      print('Не високосный')
+    else
+      print('Високосный');
+  end;
+end.
