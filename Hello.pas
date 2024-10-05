1949529325a10ecbe9dbac0a833e53d4a61adea7
kiboiko <kirboy2013@gmail.com>
2024-10-05 08:50:13 +0300

    new hello world

--- Hello.pas
+++ Hello.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  println('Hello world');
+  var time := ReadInteger('Какой час?');
+  case time of
+    4..10:Print('Доброе утро, мир!');
+    11..16:Print('Добрый вечер, мир!');
+    17..22:print('Доброй ночи, мир!');
+  end;
 end.
